2b75d6b3f041d89b7b90df0b5f6ba451a5b2a033
ShunOmine <shun12baseball@gmail.com>
2019-08-04 09:01:58 +0900

    first commit

---
+++ swipeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swipeApp
+//
+//  Created by 大嶺舜 on 2019/08/04.
+//  Copyright © 2019 大嶺舜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
